6202776332fdd4be90005146f232e88f2e45d31d
bnelson <bnelsonemail@icloud.com>
2025-02-09 19:50:42 -0500

    added methods addEdge, removeEdge and removeVertex

--- javascript/graph.js
+++ javascript/graph.js
@@ -27,13 +27,27 @@ class Graph {
   }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    if (this.nodes.has(v1) && this.nodes.has(v2)) {
+      v1.adjacent.add(v2);  // Add v2 to v1's adjacency set
+      v2.adjacent.add(v1);  // Add v1 to v2's adjacency set
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    if (this.nodes.has(v1) && this.nodes.has(v2)) {
+      v1.adjacent.delete(v2);  // Delete v2 to v1's adjacency set
+      v2.adjacent.delete(v1);  // Delete v1 to v2's adjacency set
+    }
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    if (this.nodes.has(vertex)){
+      this.nodes.delete(vertex)
+    }
+  }
 
   // this function returns an array of Node values using DFS
   depthFirstSearch(start) {}
@@ -41,5 +55,29 @@ class Graph {
   // this function returns an array of Node values using BFS
   breadthFirstSearch(start) {}
 }
+
+
+// Testing of adding edges
+let g = new Graph();  // Create a graph
+
+let a = new Node("A");  // Create node A
+let b = new Node("B");  // Create node B
+
+g.addVertex(a);  // Add node A to the graph
+g.addVertex(b);  // Add node B to the graph
+
+g.addEdge(a, b);  // Add an edge between A and B
+
+console.log(a.adjacent); // Expected output: Set { Node { value: 'B', adjacent: Set { [Circular] } } }
+console.log(b.adjacent); // Expected output: Set { Node { value: 'A', adjacent: Set { [Circular] } } }
+
+let c = new Node("C");
+g.addVertex(c);
+g.addEdge(a, c);
+
+console.log(a.adjacent); // Should now contain {B, C}
+console.log(c.adjacent); // Should contain {A}
+
+
 
 module.exports = {Graph, Node}
